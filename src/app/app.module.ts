99036ba685c5c7316cb03bcb36cf0721aeb40261
exsaro <saravanan.rsn@gmail.com>
2018-12-22 17:50:29 +0530

    adding login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { ListproductComponent } from './admin/listproduct/listproduct.component'
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
+// import { Register } from './register.model';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,15 +10,25 @@ import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  // tslint:disable-next-line:max-line-length
+  emailpattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 
   constructor(private renderer: Renderer2) {
     this.renderer.addClass(document.body, 'login');
    }
 
 
-  ngOnInit() {
+
+  registerForm(form?: NgForm) {
+    console.log(form);
+
   }
 
+  ngOnInit() {
+    this.registerForm();
+  }
   ngOnDestroy() {
     this.renderer.removeClass(document.body, 'login');
   }
