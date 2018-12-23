6e2440a0d6038ad72d80cd646acd90e034927e0e
Haiharan <hari.ct2@gmail.com>
2018-12-23 19:36:32 +0530

    Adding PHP Api
    Adding PHP Api

---
+++ src/public/index.php
@@ -0,0 +1,47 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+//require '../../vendor/autoload.php';
+require '../lib/vendor/autoload.php';
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'root';
+$config['db']['pass']   = 'Welcome@747';
+$config['db']['dbname'] = 'atozservice_db';
+
+$app = new \Slim\App(['settings' => $config]);
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+$container['signup'] = function ($container) {
+    $myService = new MyService();
+    return $myService;
+};
+$app->post('/signup', function (Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
+   /* $this->logger->addInfo("Ticket list");
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();*/
+    $response->getBody()->write("Hello, $data");
+
+    return $response;
+});
+
+$app->run();
+?>
