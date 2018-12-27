1dd2b352f863fd66a115332cf55d4c41e91653aa
Haiharan <hari.ct2@gmail.com>
2018-12-27 21:55:09 +0530

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -31,20 +31,56 @@ $container['db'] = function ($c) {
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
-$container['signup'] = function ($container) {
-    $myService = new MyService();
-    return $myService;
-};
-$app->post('/signup', function (Request $request, Response $response, array $args) {
-    $req = $request->getParsedBody();
-    $data = json_decode($req, true);
+$app->group('/api', function () use ($app) {
+    // Version group
+    $app->group('/v1', function () use ($app) {
+		//$app->get('/employees', 'getEmployes');
+		//$app->get('/employee/{id}', 'getEmployee');
+		$app->post('/create', 'addcustomer');
+		//$app->put('/update/{id}', 'updateEmployee');
+		//$app->delete('/delete/{id}', 'deleteEmployee');
+	});
+});
+function getConnection() {
+    $dbhost="localhost";
+    $dbuser="root";
+    $dbpass="Welcome@747";
+    $dbname="atozservice_db";
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $dbh;
+}
+/*$app->post('/signup', function (Request $request, Response $response, array $args) {
+   $req =json_decode($request->getBody());
+
+
+    // $data = json_decode($req, true);
        /* $this->logger->addInfo("Ticket list");
     $mapper = new TicketMapper($this->db);
-    $tickets = $mapper->getTickets();*/
-    $response->getBody()->write("Hello,".$data["name"]);
+    $tickets = $mapper->getTickets();
+    $response->withJson($req);
 
-    return $response;
-});
-
+    return $req;
+});*/
+function addcustomer($request) {
+    $cust = json_decode($request->getBody());
+	$response = array();
+    $sql = "INSERT INTO cust_sign_tbl (fname, lname, email_id,mobile) VALUES (:fname, :lname, :email, :mobile)";
+    try {
+     $db = getConnection();
+        $stmt =$db->prepare($sql);
+        $stmt->bindParam("fname", $cust->fname);
+        $stmt->bindParam("lname", $cust->lname);
+        $stmt->bindParam("email", $cust->email);
+        $stmt->bindParam("mobile", $cust->mobile);
+        $stmt->execute();
+        $response["id"] = $db->lastInsertId();
+       $db = null;
+        echo json_encode($response);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
 $app->run();
 ?>
